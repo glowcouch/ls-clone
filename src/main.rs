3f3be22ca87849e8db2eb07c26491642b373e859
glowcouch <42631203+glowcouch@users.noreply.github.com>
2024-02-22 05:21:52 +0000

    feat: add recursive capability to list_dir

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ use std::{
     path::{Path, PathBuf},
 };
 
+static INDENT_WIDTH: i32 = 2;
+
 mod colors;
 mod icons;
 mod labels;
 
 fn main() {
-    list_dir(&get_dir(), 0);
+    list_dir(&get_dir(), 0, false, 0);
 }
 
 // List the files in a directory
-fn list_dir(dir: &PathBuf, indentation: i32) {
+fn list_dir(dir: &PathBuf, indentation: i32, recursive: bool, depth: i32) {
     for entry in fs::read_dir(dir).unwrap() {
         for _ in 0..indentation {
             print!(" ")
@@ -22,9 +24,17 @@ fn list_dir(dir: &PathBuf, indentation: i32) {
             icons::icon_from_file(&entry.as_ref().unwrap().path()),
             colors::colorize_label(
                 &labels::label_fom_path(&entry.as_ref().unwrap().path()),
-                &entry.unwrap().path()
+                &entry.as_ref().unwrap().path()
             )
         );
+        if entry.as_ref().unwrap().path().is_dir() && recursive && (depth > 0 || depth < 0) {
+            list_dir(
+                &entry.unwrap().path(),
+                indentation + INDENT_WIDTH,
+                true,
+                depth - 1,
+            );
+        }
     }
 }
 

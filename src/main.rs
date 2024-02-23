8dffe65fd5614c814b01929cc94387e9a71fb6ab
glowcouch <42631203+glowcouch@users.noreply.github.com>
2024-02-23 09:45:40 +0000

    feat: hide dotfiles unless -a --all flag is passed

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,11 @@ fn main() {
 fn from_arguments() {
     let matches = command!()
         .arg(arg!([path] "Optional path (defaults to current directory)"))
+        .arg(arg!(-a --all "Don't ignore entries starting with ."))
         .arg(arg!(-r --recursive <depth> "Recursively lists files"))
         .get_matches();
+
+    let hide_dots: bool = *matches.get_one::<bool>("all").unwrap();
 
     let dir: PathBuf;
 
@@ -27,33 +30,47 @@ fn from_arguments() {
     }
     if let Some(recursive) = matches.get_one::<String>("recursive") {
         let depth = recursive.parse::<i32>();
-        list_dir(&dir, 0, true, depth.clone().unwrap());
+        list_dir(&dir, 0, true, depth.clone().unwrap(), hide_dots);
     } else {
-        list_dir(&dir, 0, false, 0);
+        list_dir(&dir, 0, false, 0, hide_dots);
     }
 }
 
 // List the files in a directory
-fn list_dir(dir: &PathBuf, indentation: i32, recursive: bool, depth: i32) {
+fn list_dir(dir: &PathBuf, indentation: i32, recursive: bool, depth: i32, hide_dots: bool) {
     for entry in fs::read_dir(dir).unwrap() {
-        for _ in 0..indentation {
-            print!(" ")
-        }
-        println!(
-            "{} {}",
-            icons::icon_from_file(&entry.as_ref().unwrap().path()),
-            colors::colorize_label(
-                &labels::label_fom_path(&entry.as_ref().unwrap().path()),
-                &entry.as_ref().unwrap().path()
-            )
-        );
-        if entry.as_ref().unwrap().path().is_dir() && recursive && (depth > 0 || depth < 0) {
-            list_dir(
-                &entry.unwrap().path(),
-                indentation + INDENT_WIDTH,
-                true,
-                depth - 1,
+        if (!entry
+            .as_ref()
+            .unwrap()
+            .path()
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .starts_with(".")
+            && !hide_dots)
+            || hide_dots
+        {
+            for _ in 0..indentation {
+                print!(" ")
+            }
+            println!(
+                "{} {}",
+                icons::icon_from_file(&entry.as_ref().unwrap().path()),
+                colors::colorize_label(
+                    &labels::label_fom_path(&entry.as_ref().unwrap().path()),
+                    &entry.as_ref().unwrap().path()
+                )
             );
+            if entry.as_ref().unwrap().path().is_dir() && recursive && (depth > 0 || depth < 0) {
+                list_dir(
+                    &entry.unwrap().path(),
+                    indentation + INDENT_WIDTH,
+                    true,
+                    depth - 1,
+                    hide_dots,
+                );
+            }
         }
     }
 }
